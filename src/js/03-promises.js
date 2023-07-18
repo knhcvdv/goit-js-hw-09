6d774c3a0eebbf0acc890e069a99e1aab535ebe9
VdovychenkoKatia <vdovychenkokatia430@gmail.com>
2023-07-18 23:50:42 +0300

    ex-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -3,14 +3,18 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   return new Promise((resolve, reject) => {
-  if (shouldResolve) {
-    // Fulfill
-    resolve({position, delay});
-  } else {
-    // Reject
-    reject({position, delay})
-  }
-  },delay)
+    if (shouldResolve) {
+      // Fulfill
+      setTimeout(() => {
+        resolve({ position, delay });
+      }, delay);
+    } else {
+      // Reject
+      setTimeout(() => {
+        reject({ position, delay });
+      }, delay);
+    }
+  });
 }
 
 const form = document.querySelector(".form")
